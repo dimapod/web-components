2adc995015dfa93a40db8c833d42f5cd1ac4de36
Dmytro Podyachiy <dimapog@gmail.com>
2015-01-11 15:08:13 +0100

    Update 2015-01-11T14:08:12.847Z

--- components/plop-button.js
+++ components/plop-button.js
@@ -1,13 +1,10 @@
 (function() {
 	"use strict";
-	console.log("foo-button register");
-
 	var PlopButtonProto = Object.create(HTMLElement.prototype);
 	PlopButtonProto.foo = function() {
 		alert('foo() called');
 	};
 	PlopButtonProto.createdCallback = function() {
-		console.log("x-foo createdCallback ");
 		var shadow = this.createShadowRoot();
 		shadow.innerHTML = "<style>b { color: green; border: 1px solid;	padding: 5px; }</style><b>I'm a SHADOW button</b>";
 
@@ -16,13 +13,9 @@
 		});
 	};
 	PlopButtonProto.attachedCallback = function() {
-		console.log("plop-button attachedCallback ")
 	};
 
 	Object.defineProperty(PlopButtonProto, "bar", {value: 5});
 
-
 	document.registerElement('plop-button', {prototype: PlopButtonProto});
-
-
 }());
