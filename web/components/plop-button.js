feaf6d8e1ac01d1091cda56f032d1477e6fecdad
Dmytro Podyachiy <dimapog@gmail.com>
2015-01-20 00:14:02 +0100

    Add some communication tests

--- web/components/plop-button.js
+++ web/components/plop-button.js
@@ -2,10 +2,11 @@
 	"use strict";
 	var PlopButtonProto = Object.create(HTMLElement.prototype);
 	PlopButtonProto.foo = function(name) {
-		alert('foo() called: ' + name);
+		this.shadow.innerHTML += "<b>+</b>";
 	};
 	PlopButtonProto.createdCallback = function() {
 		var shadow = this.createShadowRoot();
+		this.shadow = shadow;
 		shadow.innerHTML = "<style>b { color: green; border: 1px solid;	padding: 5px; }</style><b>I'm a SHADOW button</b>";
 
 		this.addEventListener('click', function(e) {
@@ -14,6 +15,10 @@
 		});
 	};
 	PlopButtonProto.attachedCallback = function() {
+	};
+	PlopButtonProto.attributeChangedCallback = function(attrName, oldVal, newVal) {
+		console.log('[plop-button] attributeChangedCallback', attrName, oldVal, newVal);
+		this.shadow.innerHTML = "<b>+</b>" + this.shadow.innerHTML;
 	};
 
 	Object.defineProperty(PlopButtonProto, "bar", {value: 5});
