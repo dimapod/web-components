f93296f89ce09e73d9914384d49c805896abf8ae
dimapod <dimapod@gmail.com>
2015-01-23 22:55:19 +0100

    Update 2015-01-23T21:55:15.762Z

--- components/plop-button.js
+++ components/plop-button.js
@@ -1,18 +1,24 @@
 (function() {
 	"use strict";
 	var PlopButtonProto = Object.create(HTMLElement.prototype);
-	PlopButtonProto.foo = function() {
-		alert('foo() called');
+	PlopButtonProto.foo = function(name) {
+		this.shadow.innerHTML += "<b>+</b>";
 	};
 	PlopButtonProto.createdCallback = function() {
 		var shadow = this.createShadowRoot();
+		this.shadow = shadow;
 		shadow.innerHTML = "<style>b { color: green; border: 1px solid;	padding: 5px; }</style><b>I'm a SHADOW button</b>";
 
 		this.addEventListener('click', function(e) {
-			alert('PLOP!');
+			console.log('event intern', e);
+			this.dispatchEvent(new Event('build'));
 		});
 	};
 	PlopButtonProto.attachedCallback = function() {
+	};
+	PlopButtonProto.attributeChangedCallback = function(attrName, oldVal, newVal) {
+		console.log('[plop-button] attributeChangedCallback', attrName, oldVal, newVal);
+		this.shadow.innerHTML = "<b>+</b>" + this.shadow.innerHTML;
 	};
 
 	Object.defineProperty(PlopButtonProto, "bar", {value: 5});
